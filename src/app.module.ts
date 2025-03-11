a2a0d56862f7985cc16003cf899c636f29e15865
leemhoon00 <leemhoon000@gmail.com>
2025-03-12 01:42:06 +0900

    setting global pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { globalPipe } from './core/pipe/global.pipe';
 
 @Module({
   imports: [],
+  providers: [globalPipe],
 })
 export class AppModule {}
